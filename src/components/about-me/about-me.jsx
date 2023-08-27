8adf092cca67eeaecb74bf4a850265724db20997
tiagofbsantos <santosfbtiago@gmail.com>
2023-08-27 22:50:10 +0100

    updated job title.

--- src/components/about-me/about-me.jsx
+++ src/components/about-me/about-me.jsx
@@ -8,7 +8,7 @@ const AboutMe = () => {
       <img className="profilePic" alt="Tiago Santos" src={ProfilePic} />
       <h1>Hi, I'm Tiago!</h1>
       <p>
-        A Full Stack Software Engineer with a Bachelor's in Computer Science and
+        Lead Software Engineer with a Bsc. in Computer Science and
         Business Management
       </p>
       <p>I build web applications in Typescript, Node.js and React</p>
